5fbe719cdbf14d37fd5c49e829c92e8da2730a81
pervin hasanov <pervinhasanov.ph@gmail.com>
2022-05-26 09:51:55 +0400

    firstCoding

---
+++ coding/firstProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Salamlama
+			Console.WriteLine( "Salam xos gelmisiniz");
+			Console.Write( "adinizi daxil edin");
+			Console.ReadLine();
+			#endregion
+
+			#region task1
+			var red = "qirmizi";
+			var car = "avtomobil";
+			var expensive = "bahali";
+			var phone = "telefon";
+
+			Console.WriteLine(red + " " + car);
+			Console.WriteLine(expensive + " " +phone);
+			#endregion
+
+			#region deyisen teyini
+			string reng = "sari";
+			Console.WriteLine(reng);
+			#endregion
+			Console.ReadLine();
+
+		}
+	}
+}
